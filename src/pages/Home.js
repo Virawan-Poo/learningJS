1147466813e1c26d473c8e17384a351a1e17b2d8
Virawan.Poo <l3elovepoo@gmail.com>
2023-10-05 15:33:32 +0700

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,131 +1,148 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const ENUM_TEXT = {
-  DEFAULT: 'Defualt Value',
-  COUNTING: 'Counting!',
-  MAXVALUE: 'Max Value!',
-}
+  DEFAULT: "Defualt Value",
+  COUNTING: "Counting!",
+  MAXVALUE: "Max Value!",
+};
 
 const ENUM_ACTION = {
-  DECRESE: '-',
-  INCREASE: '+',
-}
+  DECRESE: "-",
+  INCREASE: "+",
+};
 
 const Home = () => {
-  const numberArr = [1, 2, 3, 4, 5, 6, 7]
+  const numberArr = [1, 2, 3, 4, 5, 6, 7];
 
   const multiplyEvensNumber = (numberList) => {
     if (numberList.length) {
-      let evenNumbersForMultiply = numberList.filter((num) => num % 2 === 0)
+      let evenNumbersForMultiply = numberList.filter((num) => num % 2 === 0);
       return console.log(
-        'multiplay Number is',
+        "multiplay Number is",
         evenNumbersForMultiply.map((even) => even * 2)
-      )
+      );
       //return console.log('multiplay Number is', evenNumbersForMultiply)
     }
-    return console.log('Error')
-  }
+    return console.log("Error");
+  };
 
-  multiplyEvensNumber(numberArr)
+  multiplyEvensNumber(numberArr);
 
   const userData = [
     {
-      name: 'Win',
+      name: "Win",
       age: 25,
     },
     {
-      name: 'Ton',
+      name: "Ton",
       age: 25,
     },
     {
-      name: 'Jeff',
+      name: "Jeff",
       age: 33,
     },
     {
-      name: 'Boat',
+      name: "Boat",
       age: 26,
     },
-  ]
+  ];
 
   const getAgeByName = (name) => {
     let result = {
       age: 0,
-      name: '',
-    }
+      name: "",
+    };
     userData.filter((data) => {
       if (data.name === name) {
-        result = data
+        result = data;
       }
-      return result
-    })
-    return console.log(name, 'Age is :', result.age)
-  }
+      return result;
+    });
+    return console.log(name, "Age is :", result.age);
+  };
 
   const groupUserByAge = () => {
-    const groupAge = userData.reduce((prev, cur) => ({ ...prev, [cur.age]: (prev[cur.age] || []).concat(cur.name) }), {})
-    return console.log('groupAge', groupAge)
-  }
+    const groupAge = userData.reduce(
+      (prev, cur) => ({
+        ...prev,
+        [cur.age]: (prev[cur.age] || []).concat(cur.name),
+      }),
+      {}
+    );
+    return console.log("groupAge", groupAge);
+  };
 
   const getUserByMaxAndMinAge = (action) => {
     let result = {
-      name: '',
+      name: "",
       age: 0,
+    };
+
+    if (action === "max") {
+      result = userData.reduce((prev, current) =>
+        prev.age > current.age ? prev : current
+      );
+      return console.log("maxAge!!!!", result.name);
     }
 
-    if (action === 'max') {
-      result = userData.reduce((prev, current) => (prev.age > current.age ? prev : current))
-      return console.log('maxAge!!!!', result.name)
+    if (action === "min") {
+      result = userData.reduce((prev, current) =>
+        prev.age < current.age ? prev : current
+      );
+      return console.log("minAge!!!!", result.name);
     }
+    return console.log("ERROR");
+  };
 
-    if (action === 'min') {
-      result = userData.reduce((prev, current) => (prev.age < current.age ? prev : current))
-      return console.log('minAge!!!!', result.name)
-    }
-    return console.log('ERROR')
-  }
+  getUserByMaxAndMinAge("min");
+  getUserByMaxAndMinAge("max");
+  getAgeByName("Boat");
+  getAgeByName("Jeff");
+  groupUserByAge();
 
-  getUserByMaxAndMinAge('min')
-  getUserByMaxAndMinAge('max')
-  getAgeByName('Boat')
-  groupUserByAge()
-
-  const [count, setCount] = useState(0)
-  const [text, setText] = useState(ENUM_TEXT.DEFAULT)
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState(ENUM_TEXT.DEFAULT);
 
   const handleTotal = (action) => {
     if (action === ENUM_ACTION.DECRESE) {
-      setCount(count - 1)
+      setCount(count - 1);
     }
 
     if (action === ENUM_ACTION.INCREASE) {
-      setCount(count + 1)
+      setCount(count + 1);
     }
-  }
+  };
 
   useEffect(() => {
     if (count >= 1) {
-      setText(ENUM_TEXT.COUNTING)
+      setText(ENUM_TEXT.COUNTING);
     }
     if (count === 10) {
-      setText(ENUM_TEXT.MAXVALUE)
+      setText(ENUM_TEXT.MAXVALUE);
     }
     if (count === 0) {
-      setText(ENUM_TEXT.DEFAULT)
+      setText(ENUM_TEXT.DEFAULT);
     }
-  }, [count])
+  }, [count]);
 
   return (
     <div>
-      <button onClick={() => handleTotal(ENUM_ACTION.DECRESE)} disabled={count <= 0}>
+      <button
+        onClick={() => handleTotal(ENUM_ACTION.DECRESE)}
+        disabled={count <= 0}
+      >
         -
       </button>
       <span>{count}</span> <span>{text}</span>
-      <button onClick={() => handleTotal(ENUM_ACTION.INCREASE)} disabled={count === 10}>
+      <button
+        onClick={() => handleTotal(ENUM_ACTION.INCREASE)}
+        disabled={count === 10}
+      >
         +
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
